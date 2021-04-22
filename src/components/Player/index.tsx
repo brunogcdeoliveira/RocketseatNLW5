7c5f739739801439cddc0bdadb92b13fda462584
Bruno de Oliveira <43137306+brunogcdeoliveira@users.noreply.github.com>
2021-04-22 14:29:47 -0300

    audio tag inserted

--- src/components/Player/index.tsx
+++ src/components/Player/index.tsx
@@ -14,14 +14,14 @@ export function Player() {
     const episode = episodeList[currentEpisodeIndex]
     
     return (
-       <div className={styles.playerContainer}>
-           <header>
+        <div className={styles.playerContainer}>
+            <header>
                <img src="/playing.svg" alt="Tocando agora"/>
                <strong>Tocando agora</strong>
-           </header>
+            </header>
 
-           { episode ? (
-               <div className={styles.currentEpisode}>
+            { episode ? (
+                <div className={styles.currentEpisode}>
                    <Image  
                     width={592}
                     height={592}
@@ -30,17 +30,17 @@ export function Player() {
                    />
                    <strong>{episode.title}</strong>
                    <span>{episode.members}</span>
-               </div>
-           ) : (
+                </div>
+            ) : (
             <div className={styles.emptyPlayer}>
                 <strong>Selecione um podcast para ouvir</strong>
             </div>
-           ) }
+            ) }
 
-           <footer className={!episode ? styles.empty : ''}>
-               <div className={styles.progress}>
-                   <span>00:00</span>
-                   <div className={styles.slider}>
+            <footer className={!episode ? styles.empty : ''}>
+                <div className={styles.progress}>
+                    <span>00:00</span>
+                    <div className={styles.slider}>
                     { episode ? (
                         <Slider 
                             trackStyle={{ backgroundColor: '#04d361' }}
@@ -50,29 +50,35 @@ export function Player() {
                     ) : (
                         <div className={styles.emptySlider} />
                     ) }
-                   </div>
-                   <span>00:00</span>
-               </div>
+                    </div>
+                    <span>00:00</span>
+                </div>
 
-               <div className={styles.buttons}>
-                   <button type="button" disabled={!episode}>
+                { episode && (
+                    <audio 
+                        src={episode.url}
+                        autoPlay
+                    />
+                 ) }
+
+                <div className={styles.buttons}>
+                    <button type="button" disabled={!episode}>
                        <img src="/shuffle.svg" alt="Embaralhar"/>
-                   </button>
-                   <button type="button" disabled={!episode}>
+                    </button>
+                    <button type="button" disabled={!episode}>
                        <img src="/play-previous.svg" alt="Tocar anterior"/>
-                   </button>
-                   <button type="button" className={styles.playButton} disabled={!episode}>
+                    </button>
+                    <button type="button" className={styles.playButton} disabled={!episode}>
                        <img src="/play.svg" alt="Tocar"/>
-                   </button>
-                   <button type="button" disabled={!episode}>
+                    </button>
+                    <button type="button" disabled={!episode}>
                        <img src="/play-next.svg" alt="Tocar próxima"/>
-                   </button>
-                   <button type="button" disabled={!episode}>
+                    </button>
+                    <button type="button" disabled={!episode}>
                        <img src="/repeat.svg" alt="Repetir"/>
-                   </button>
-
-               </div>
-           </footer>
-       </div>
+                    </button>
+                </div>
+            </footer>
+        </div>
     );
 }
