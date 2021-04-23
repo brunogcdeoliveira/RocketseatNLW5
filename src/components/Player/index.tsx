e94bffc6305aacc1d8af77d3c8321975434fa8bb
Bruno de Oliveira <43137306+brunogcdeoliveira@users.noreply.github.com>
2021-04-23 15:34:47 -0300

    play next and play previous implemented

--- src/components/Player/index.tsx
+++ src/components/Player/index.tsx
@@ -16,7 +16,9 @@ export function Player() {
         currentEpisodeIndex, 
         isPlaying, 
         togglePlay ,
-        setPlayingState
+        setPlayingState,
+        playNext,
+        playPrevious
     } = useContext(PlayerContext)
     
     useEffect(() => {
@@ -88,7 +90,7 @@ export function Player() {
                     <button type="button" disabled={!episode}>
                        <img src="/shuffle.svg" alt="Embaralhar"/>
                     </button>
-                    <button type="button" disabled={!episode}>
+                    <button type="button" onClick={playPrevious} disabled={!episode}>
                        <img src="/play-previous.svg" alt="Tocar anterior"/>
                     </button>
                     <button 
@@ -101,7 +103,7 @@ export function Player() {
                             ? <img src="/pause.svg" alt="Tocar" />
                             : <img src="/play.svg" alt="Tocar" /> }
                     </button>
-                    <button type="button" disabled={!episode}>
+                    <button type="button" onClick={playNext} disabled={!episode}>
                        <img src="/play-next.svg" alt="Tocar próxima"/>
                     </button>
                     <button type="button" disabled={!episode}>
--- src/contexts/PlayerContext.tsx
+++ src/contexts/PlayerContext.tsx
@@ -16,6 +16,8 @@ type PlayerContextData = {
     playList: (list: Episode[], index: number) => void;
     setPlayingState: (state: boolean) => void;
     togglePlay: () => void;
+    playNext: () => void;
+    playPrevious: () => void;
 };
 
 export const PlayerContext = createContext({} as PlayerContextData);
@@ -49,6 +51,20 @@ export function PlayerContextProvider({ children }: PlayerContextProviderProps) 
     setIsPlaying(state);
     }     
 
+    function playNext() {
+        const nextEpisodeIndex = currentEpisodeIndex + 1;
+
+        if (nextEpisodeIndex < episodeList.length) {
+            setCurrentEpisodeIndex(currentEpisodeIndex + 1);
+        }
+    }
+
+    function playPrevious() {
+        if (currentEpisodeIndex > 0) {
+            setCurrentEpisodeIndex(currentEpisodeIndex - 1);
+        }
+    }
+
     return (
         <PlayerContext.Provider 
         value={{ 
@@ -56,6 +72,8 @@ export function PlayerContextProvider({ children }: PlayerContextProviderProps) 
             currentEpisodeIndex, 
             play,
             playList, 
+            playNext,
+            playPrevious,
             isPlaying, 
             togglePlay, 
             setPlayingState 
