edf559347586335d375df5b8d8fe91cce65f1ba2
Bruno de Oliveira <43137306+brunogcdeoliveira@users.noreply.github.com>
2021-04-24 13:12:49 -0300

    slider implemented

--- src/components/Player/index.tsx
+++ src/components/Player/index.tsx
@@ -26,7 +26,8 @@ export function Player() {
         playNext,
         playPrevious,
         hasNext,
-        hasPrevious
+        hasPrevious,
+        clearPlayerState
     } = usePlayer();
     
     useEffect(() => {
@@ -47,6 +48,19 @@ export function Player() {
         audioRef.current.addEventListener('timeupdate', () => {
             setProgress(Math.floor(audioRef.current.currentTime));
         })
+    }
+    
+    function handleSeek(amount: number) {
+        audioRef.current.currentTime = amount;
+        setProgress(amount);
+    }
+    
+    function handleEpisodeEnded() {
+        if (hasNext) {
+            playNext()
+        } else {
+            clearPlayerState()
+        }
     }
     
     const episode = episodeList[currentEpisodeIndex]
@@ -83,6 +97,7 @@ export function Player() {
                         <Slider 
                             max={episode.duration}
                             value={progress}
+                            onChange={handleSeek}
                             trackStyle={{ backgroundColor: '#04d361' }}
                             railStyle={{ backgroundColor: '#9f75ff' }}
                             handleStyle={{ borderColor: '#04d361', borderWidth: 4 }}
@@ -99,6 +114,7 @@ export function Player() {
                         src={episode.url}
                         ref={audioRef}
                         autoPlay
+                        onEnded={handleEpisodeEnded}
                         loop={isLooping}
                         onPlay={() => setPlayingState(true)}
                         onPause={() => setPlayingState(false)}
